39a483d6abd037adf30605b8340c09dab66c3f3c
AlynxZhou <alynx.zhou@gmail.com>
2018-03-19 20:49:40 +0800

    Site updated by hexo at 2018-03-19 20:49:39.

--- js/animate.js
+++ js/animate.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 19 2018 19:52:51 GMT+0800 (CST)
+// build time:Mon Mar 19 2018 20:49:36 GMT+0800 (CST)
 "use strict";(function(e){e(document).ready(function(){var a=300;var r={opacity:1,top:0};function t(){var r=e.Deferred();var t=e(".animate");var n=t.length;t.each(function(t,c){e(c).delay(t*a).addClass("active");n-1===t&&r.resolve()});return r.promise()}t()})})(jQuery);
 //rebuild by neat 
