aa9caa86733cbaeb6ad058351496391b5d1755e6
Alynx Zhou <alynx.zhou@gmail.com>
2019-09-14 22:11:01 +0800

    Updated site.

--- docs/posts/Learn-OpenGL-1/triangle.f.glsl
+++
@@ -1,8 +0,0 @@
-#version 300 es
-precision mediump float;
-in vec3 pass_color;
-out vec4 frag_color;
-void main()
-{
-	frag_color = vec4(pass_color, 1.0f);
-}
--- docs/posts/Learn-OpenGL-1/triangle.v.glsl
+++
@@ -1,9 +0,0 @@
-#version 300 es
-layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 color;
-out vec3 pass_color;
-void main()
-{
-	gl_Position = vec4(pos, 1.0f);
-	pass_color = color;
-}
